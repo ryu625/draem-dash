f261f74857250a5bcf08d7188b1d2af5aa9b2149
ryu625 <260798180+ryu625@users.noreply.github.com>
2026-07-06 18:12:46 +0900

    Make app milestones authoritative over sync

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const V='v117';
+const V='v118';
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(V).then(c=>c.addAll(['./index.html','./','./manifest.json','./music-hub.html','./music-hub-sync.json'])));
   self.skipWaiting();
