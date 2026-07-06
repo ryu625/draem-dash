3b1d7169a0a727c06d96a0e4bc88a7e974f65dc8
ryu625 <260798180+ryu625@users.noreply.github.com>
2026-07-06 18:36:50 +0900

    Make mobile repair link more robust

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const V='v121';
+const V='v122';
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(V).then(c=>c.addAll(['./index.html','./','./manifest.json','./music-hub.html','./music-hub-sync.json','./repair.html'])));
   self.skipWaiting();
