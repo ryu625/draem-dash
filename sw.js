4b11642331f10b3c1c9e6f3c3eeb6e302969e2bc
ryu625 <260798180+ryu625@users.noreply.github.com>
2026-07-13 01:29:01 +0800

    feat: add always-on mobile agent queue

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const V='v122';
+const V='v126';
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(V).then(c=>c.addAll(['./index.html','./','./manifest.json','./music-hub.html','./music-hub-sync.json','./repair.html'])));
   self.skipWaiting();
