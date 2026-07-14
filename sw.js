e1e0c16009b986f61696e794a9b952454b54372a
ryu625 <260798180+ryu625@users.noreply.github.com>
2026-07-14 10:56:35 +0900

    fix: show verified always-on sync state

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const V='v127';
+const V='v128';
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(V).then(c=>c.addAll(['./index.html','./','./manifest.json','./music-hub.html','./music-hub-sync.json','./repair.html'])));
   self.skipWaiting();
